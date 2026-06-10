201a6457f578ccce138fec71685e5014856e7fe3
UltralyticsAssistant <web@ultralytics.com>
2026-06-10 16:20:48 +0000

    Auto-format by https://ultralytics.com/actions

--- detect_main.py
+++ detect_main.py
@@ -1,14 +1,17 @@
-import sys
-from ultralytics import YOLO
 import os
+import sys
+
+from ultralytics import YOLO
+
 
 # 打包动态路径
 def get_model_path():
-    if hasattr(sys, '_MEIPASS'):
+    if hasattr(sys, "_MEIPASS"):
         return os.path.join(sys._MEIPASS, "best.pt")
     return "best.pt"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     model = YOLO(get_model_path())
     # 测试digit数据集图片
     res = model.predict(source="digit_dataset/images", save=True, conf=0.03)
